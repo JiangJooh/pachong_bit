7c53906e436c543919c30e897d584d6a629880a6
lv0817 <412435484@qq.com>
2018-02-09 23:31:09 +0800

    完成 爬取

--- bit_shili_taobao.py
+++ bit_shili_taobao.py
@@ -5,13 +5,40 @@ import requests
 import re
 
 def getHTMLText(url):#从url获取html信息
-    pass 
+    try:
+        r = requests.get(url,timeout=30)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding #将整体的编码替换成文本主体的编码
+        print('完成html页面读取')
+        return r.text
+    except:
+        return "error"
+    
 
+
+'''本例中没有使用bs4，而是直接使用搜索的策略'''
 def parsePage(ilt,html):#解析页面
-    pass
+    try :
+        plt = re.findall(r'"view_price":"[\d.]*"',html)
+        tlt = re.findall(r'"raw_title":".*?"',html)
+        for i in range(len(plt)):
+            price = eval(plt[i].split(':')[1])
+            title = eval(tlt[i].split(':')[1])
+            ilt.append([price,title])
+    except:
+        print('error')
+    print('完成页面解析，已将数据存入列表')
+
 
 def printGoodList(ilt):#输出商品信息列表
-    print('')
+    tplt = '{:4}\t{:8}\t{:16}'
+    print(tplt.format('序号','价格','商品名称'))
+    count = 0
+    for g in ilt:
+        count = count+1
+        print(tplt.format(count,g[0],g[1]))
+    
+    print('打印完成')
 
 def main():
     goods = '联想' #设计搜索内容
@@ -32,9 +59,8 @@ def main():
 
 
 
-
-if '__name__' == '__main__':
+if __name__ == '__main__':
+    print('begin')
     main()
     print('主函数执行完毕')
-
 
