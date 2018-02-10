a9df095929fbc0569d6c807cec165079c37a4d15
lv0817 <412435484@qq.com>
2018-02-10 21:59:15 +0800

    实例

--- bit_shili_gupiao.py
+++ bit_shili_gupiao.py
@@ -4,18 +4,18 @@ import traceback
 import re
 
 def getHTMLText(url):
-    print('开始获取URL信息')
-    try：
+    #print('开始获取URL信息')
+    try:
         r = requests.get(url,timeout=30)
         r.raise_for_status()
         r.encoding = r.apparent_encoding
         return r.text
     except:
         return ""
-    print('获取URL信息完毕')
+    #print('获取URL信息完毕')
 
 def getStockList(lst,stockURL):
-    print('开始获取网站上的股票信息')
+    print('开始获取网站上的股票列表')
     html = getHTMLText(stockURL)
     soup = BeautifulSoup(html,'html.parser')
     a = soup.find_all('a')
@@ -31,24 +31,46 @@ def getStockList(lst,stockURL):
             #在append到lst里，这样lst就不会是[[sh201000], [sh201002]]这样
         except:
             continue
-
+    print('获取股票列表完毕')
 
 def getStockInfo(lst,stockURL,fpath):
+    print('开始获取股票信息')
     for stock in lst:
         url = stockURL+stock+'.html'
         html = getHTMLText(url)
         try:
-            if html = '':
+            if html=='':
                 continue 
             infoDict = {}
             soup = BeautifulSoup(html,'html.parser')
             stockInfo = soup.find('div',attrs={'class':'stock-bets'})#获取当前股票所在的div标签
+            print(stockInfo)
             # find 返回一个字符串
             # find_all 返回一个列表
 
             #获取股票名字
-            na
-def mian():
+            name = stockInfo.find_all(attrs = {'class':'bets-name'})
+            infoDict.update({'股票名称':name.text.split()})
+
+            keyList = stockInfo.find_all('dt')
+            valueList = stockInfo.find_all('dd')#找到股票信息的两个键、值标签
+            #将键值对还原成键值对，并存到字典中
+            for i in range(len(keyList)):
+                key = keyList[i].text
+                val = valueList[i].text
+                infoDict[key]=val
+
+            with open(fpath,'a',encoding='utf-8') as f:
+                f.write(str(infoDict)+'\n')
+
+        except:
+            traceback.print_exc()
+            continue
+    print('开始获取股票信息')
+
+
+
+def main():
     stock_list_url = 'http://quote.eastmoney.com/stocklist.html'
     stock_info_url = 'https://gupiao.baidu.com/stock/'
     output_file = 'F://BaiduStockInfo.txt'
